f65e3bceaa1664d39fadd46b6c3f48e7f20e97b4
Laetitia <laetitia.potrat@live.fr>
2021-05-12 10:54:50 +0200

    var SCSS - component heade - App.vue - Home with header

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+      <img src="../../public/img/logo-blue.png" alt="logo">
+  </div>
+</template>
+
+
+
+<script>
+export default {
+    
+    name: "Header",
+
+    
+   props: {
+
+   }
+   ,
+
+    
+    methods: {
+
+    }
+}
+</script>
+
+
+<style lang="scss">
+
+.header {
+    height: 100px;
+    background: grey;
+}
+
+
+</style>
