edf26f1930ca68b86c0bd7c00f3852afa8aa46e0
alexandre Dalmar <alexandredalmar@gmail.com>
2021-05-13 18:27:34 +0200

    page Login en cours

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Inscription from "../views/Register.vue";
+import Connexion from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,12 @@ const routes = [
     path: "/Register",
     name: "Register",
     component: Inscription,
+  },
+
+  {
+    path: "/Login",
+    name: "Login",
+    component: Connexion,
   },
 ];
 
