a9aa634fef169a83c1f65de3e1ba14b5d9fa0a87
alexandre Dalmar <alexandredalmar@gmail.com>
2021-05-14 22:32:23 +0200

    requete GET actors OK

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -104,8 +104,12 @@
 
         <div class="col-md-3">
           <label for="inputFacebook" class="form-label">Facebook</label>
-          <input v-model="facebook" type="text" class="form-control"
-          id="inputFacebook"
+          <input
+            v-model="facebook"
+            type="text"
+            class="form-control"
+            id="inputFacebook"
+          />
         </div>
 
         <div class="col-md-3">
