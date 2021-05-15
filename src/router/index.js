bb7dc74b29c81e44fc15e29c5e3cf917ff8d7e67
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-05-15 23:59:27 +0200

    recup du token

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Admin from "../views/Admin.vue";
 import Connexion from "../views/Login.vue";
 import Personelle from "../views/Personal.vue";
 import Test from "../views/testInfo.vue";
+import Token from "../views/Token.vue";
 
 Vue.use(VueRouter);
 
@@ -44,6 +45,7 @@ const routes = [
     name: "Personal",
     component: Personelle,
   },
+  { path: "/getToken/:ml/:id", component: Token, props: true },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Token.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="token"></div>
+</template>
+
+<script>
+export default {
+  inject: ["baseUrl"],
+  name: "token",
+  props: ["ml", "id"],
+  created() {
+    let url = `${this.baseUrl}api/GET/login/${this.ml}/${this.id}`;
+    this.axios.get(url).then((response) => {
+      console.log(response.data.token);
+    });
+  },
+};
+</script>
+
+<style lang="scss"></style>
