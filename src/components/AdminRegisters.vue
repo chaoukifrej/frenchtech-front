10cb3ede998b31367a05f1057b7465001d5c93c4
Laetitia <laetitia.potrat@live.fr>
2021-05-17 11:46:10 +0200

    Register buffer

--- src/components/AdminRegisters.vue
+++ src/components/AdminRegisters.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-
-
+  <div class="mainComponent">
+    <div class="blocCards">
+        <div class="displayAdminRegisters" v-for="item in dataTab" :key="item">
+          <div v-for="index in item" :key="index.id">
+            <!-- i correspond a la props / index correspond a l'iteration du 2 Tab -->
+            <CardInfo :i="index" />
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -10,111 +16,50 @@ export default {
   name: "Register",
   inject: ["baseUrl"],
   data: () => ({
-    logo: "",
-    name: "",
-    email: "",
-    phone: "",
-    streetNumber: "",
-    streetName: "",
-    adress: "",
-    city: "",
-    postal_code: "",
-    website: "",
-    facebook: "",
-    twitter: "",
-    linkedin: "",
-    category: "",
-    associations: "",
-    activity_area: "",
-    description: "",
-
-    /* information visible uniquement par l'admin */
-
-    funds: "",
-    employees_number: "",
-    jobs_available_number: "",
-    women_number: "",
-    revenues: "",
-    position: "",
-    latitude: "",
-    longitude: "",
+   registerBuffer: [],
   }),
 
-  /* Permet de regrouper les differents element lié a la position dans la variable adress */
-  watch: {
-    streetName: function() {
-      this.adress = this.streetNumber + " " + this.streetName;
-    },
-
-    streetNumber: function() {
-      this.adress = this.streetNumber + " " + this.streetName;
-    },
-  },
-
   methods: {
-    getPosition() {
-      /* requete afin de recupérer les coordonnées GPS des adresses */
 
+    registerBuffer() {
       this.axios
-        .get(
-          `https://api-adresse.data.gouv.fr/search/?q=${this.streetNumber}+${this.streetName}+${this.city}+${this.postal_code}%22`
+        .get(this.baseUrl + "/api/GET/buffers")
+
+        .then(
+        (response) => (
+          this.registerBuffer.push(response.data.body.buffers),
+          console.log(this.registerBuffer)
         )
-
-        .then((response) => {
-          this.longitude = response.data.features[0].properties.x;
-          this.latitude = response.data.features[0].properties.y;
-        });
-
-      console.log(this.longitude);
+      );
     },
+},
+}
 
-    register() {
-      this.axios
-        .post(this.baseUrl + "api/POST/register", {
-          /* body de la requete */
-
-          name: this.name,
-          email: this.email,
-          logo: this.logo,
-          adress: this.adress,
-          postal_code: this.postal_code,
-          city: this.city,
-          longitude: this.longitude,
-          latitude: this.latitude,
-          phone: this.phone,
-          category: this.category,
-          associations: this.associations,
-          description: this.description,
-          facebook: this.facebook,
-          twitter: this.twitter,
-          linkedin: this.linkedin,
-          website: this.website,
-          activity_area: this.activity_area,
-          funds: this.funds,
-          employees_number: this.employees_number,
-          jobs_available_number: this.employees_number,
-          women_number: this.women_number,
-          revenues: this.revenues,
-        })
-
-        .then((response) => console.log(response));
-    },
-
-    /* methode transformer le logo en base 64 pour la BDD */
-
-    addLogo(e) {
-      const reader = new FileReader();
-      reader.onload = (readerEvent) => {
-        this.logo = readerEvent.target.result;
-      };
-      if (e.target.files[0].size / 1024 / 1024 > 3) {
-        console.log("image trop grande");
-      } else {
-        reader.readAsDataURL(e.target.files[0]);
-      }
-    },
-  },
-};
+/* body de la requete */
+            /*if (type_of_demand = ) {
+              name: this.name,
+                    email: this.email,
+                    logo: this.logo,
+                    adress: this.adress,
+                    postal_code: this.postal_code,
+                    city: this.city,
+                    longitude: this.longitude,
+                    latitude: this.latitude,
+                    phone: this.phone,
+                    category: this.category,
+                    associations: this.associations,
+                    description: this.description,
+                    facebook: this.facebook,
+                    twitter: this.twitter,
+                    linkedin: this.linkedin,
+                    website: this.website,
+                    activity_area: this.activity_area,
+                    funds: this.funds,
+                    employees_number: this.employees_number,
+                    jobs_available_number: this.employees_number,
+                    women_number: this.women_number,
+                    revenues: this.revenues
+              },*/
 </script>
 
 <style lang="scss">
@@ -140,3 +85,5 @@ export default {
   }
 }
 </style>
+
+
