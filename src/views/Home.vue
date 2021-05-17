fa89a79547fd22f970fab63fcb8eb4b4eb05ed3e
alexandre Dalmar <alexandredalmar@gmail.com>
2021-05-17 09:52:43 +0200

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,6 +32,7 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 // Import header et footer
 import Header from "@/components/Header.vue";
+import CardInfo from "@/components/CardInfo.vue";
 
 export default {
   name: "App",
@@ -42,6 +43,7 @@ export default {
     LMarker,
     LPopup,
     Header,
+    CardInfo,
   },
   data() {
     return {
