e8747c7193630dbf2dd759522a490f8902f42132
alexandre Dalmar <alexandredalmar@gmail.com>
2021-05-18 16:21:22 +0200

    sideBar ID OK

--- src/components/AdminRegister.vue
+++ src/components/AdminRegister.vue
@@ -1,51 +1,60 @@
 <template>
   <div class="mainComponent">
-    <div class="blocCards">
-      <div v-for="index in registerBuffer" :id="index.id" :key="index.id">
-        <!-- i correspond a la props / index correspond a l'iteration du 2 Tab -->
-        <div class="row">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Entreprise</th>
-                <th scope="col">E-mail</th>
-                <th scope="col">Téléphone</th>
-                <th scope="col">Adresse</th>
-                <th scope="col">Adresse</th>
-                <th scope="col">Adresse</th>
-                <th scope="col">Ville</th>
-                <th scope="col">Code Postal</th>
-                <th scope="col">Site web</th>
-                <th scope="col">Catégory</th>
-                <th scope="col">Association</th>
-                <th scope="col">Secteur d'activité</th>
-                <th scope="col">Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>{{ index.name }}</td>
-                <td>{{ index.email }}</td>
-                <td>{{ index.phone }}</td>
-                <td>{{ index.website }}</td>
-                <td>{{ index.category }}</td>
-                <td>{{ index.associations }}</td>
-              </tr>
-            </tbody>
-          </table>
-          <div class="col-md-2">
-            <span>
-              <button class="btn btn-primary">Accepter</button>
-              <button type="button" class="btn btn-warning">Modifier</button>
-              <button @click="deleteUser(index.id)" class="btn btn-danger">
-                Refuser
-              </button>
-            </span>
-          </div>
-        </div>
-      </div>
+    <div>
+      <b-tabs content-class="mt-3">
+        <b-tab title="Gestion entreprise" active
+          ><div class="blocCards">
+            <div v-for="index in registerBuffer" :id="index.id" :key="index.id">
+              <!-- i correspond a la props / index correspond a l'iteration du 2 Tab -->
+              <div class="row">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Entreprise</th>
+                      <th scope="col">E-mail</th>
+                      <th scope="col">Téléphone</th>
+                      <th scope="col">Adresse</th>
+                      <th scope="col">Ville</th>
+                      <th scope="col">Code Postal</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>{{ index.name }}</td>
+                      <td>{{ index.email }}</td>
+                      <td>{{ index.phone }}</td>
+                      <td>{{ index.adress }}</td>
+                      <td>{{ index.city }}</td>
+                      <td>{{ index.postal_code }}</td>
+
+                      <td>
+                        <button type="button" class="btn btn-warning">
+                          Modifier
+                        </button>
+                      </td>
+                      <td>
+                        <div class="col-md-2">
+                          <button
+                            @click="deleteUser(index.id)"
+                            class="btn btn-danger"
+                          >
+                            Refuser
+                          </button>
+                        </div>
+                      </td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div></div
+        ></b-tab>
+        <b-tab title="Demande d'inscription"></b-tab>
+        <b-tab title="Demande de modification"></b-tab>
+        <b-tab title="Demande de supression"></b-tab>
+      </b-tabs>
     </div>
   </div>
 </template>
