4f9b12d49ef65702830f8d1e2190fdead3a309e1
chevillon loic <chevillonloic@gmail.com>
2021-05-20 16:33:28 +0200

    axios historic fait

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -187,7 +187,6 @@ export default {
       .then((response) => {
         for (const i of response.data.body.historic) {
           this.arrayHistoric.push(response.data.body.historic);
-          console.log(response.data.body.historic);
           var monthNames = [
             "Janvier",
             "Février",
@@ -217,40 +216,6 @@ export default {
         }
         this.fillData();
       });
-
-    this.axios.get(this.baseUrl + "api/admin/GET/historic").then((response) => {
-      for (const i of response.data.body.historic) {
-        this.arrayHistoric.push(response.data.body.historic);
-        //console.log(response.data.body.historic);
-        var monthNames = [
-          "Janvier",
-          "Février",
-          "Mars",
-          "Avril",
-          "Mai",
-          "Juin",
-          "Juillet",
-          "Août",
-          "Septembre",
-          "Octobre",
-          "Novembre",
-          "Décembre",
-        ];
-        var newDate = new Date(Date.parse(i.created_at));
-        var formattedDate = monthNames[newDate.getMonth()];
-        this.date.push(formattedDate);
-        this.totalFundsHistoric.push(i.total_funds);
-        this.totalJobsHistoric.push(i.total_jobs_available);
-        this.totalRevenuesHistoric.push(i.total_revenues);
-        this.totalActorsHistoric.push(i.total_actors);
-        this.totalWomenHistoric.push(i.total_women_number);
-        this.totalEmployeesHistoric.push(i.total_employees_number);
-        this.totalMenHistoric.push(
-          i.total_employees_number - i.total_women_number
-        );
-      }
-      this.fillData();
-    });
 
     this.axios.get(this.baseUrl + "api/GET/metric").then((response) => {
       console.log(response.data.body);
