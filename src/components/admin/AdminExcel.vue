5b37154f6b244e7fcd561b6beb1703c5440cd1c2
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-05-21 11:29:58 +0200

    c'est parti mode excel

--- src/components/admin/AdminExcel.vue
+++ src/components/admin/AdminExcel.vue
@@ -23,7 +23,6 @@
       </div>
       <div class="firstRowCol">
         <h5>Demandes à valider</h5>
-        <b-button class="m-2">Liste complète ( Tout les détails )</b-button>
         <b-button class="m-2">Demande d'inscription</b-button
         ><b-button class="m-2">Demande de modification</b-button
         ><b-button class="m-2">Demande de suppression</b-button>
