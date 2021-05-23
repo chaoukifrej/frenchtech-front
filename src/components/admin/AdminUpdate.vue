b3611fa11054d9408a3f2465c3a8867375a73b10
Michael SANTINI <michael.santinimc@gmail.com>
2021-05-23 23:11:26 +0200

    ajout AdminUpdate

--- src/components/admin/AdminUpdate.vue
+++ src/components/admin/AdminUpdate.vue
@@ -1,139 +1,54 @@
 <template>
-  <div></div>
+  <div>
+    <b-table primary-key="id" striped hover :items="buffers" :fields="liste">
+      <template #cell(actions)="data">
+        <b-button pill variant="warning" size="sm" class="m-2">
+          Voir modification
+        </b-button>
+        <b-button pill variant="danger" size="sm" class="m-2">
+          Rejeter
+        </b-button>
+      </template>
+    </b-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Register",
+  name: "AdminUpdate",
   inject: ["baseUrl"],
-  data: () => ({
-    logo: "",
-    name: "",
-    email: "",
-    phone: "",
-    streetNumber: "",
-    streetName: "",
-    adress: "",
-    city: "",
-    postal_code: "",
-    website: "",
-    facebook: "",
-    twitter: "",
-    linkedin: "",
-    category: "",
-    associations: "",
-    activity_area: "",
-    description: "",
 
-    /* information visible uniquement par l'admin */
+  data() {
+    return {
+      // Tableau d'affichage
+      liste: [
+        { key: "id", label: "N°" },
+        { key: "name", label: "Nom entreprise" },
+        { key: "email", label: "E-mail" },
+        { key: "phone", label: "Téléphone" },
+        { key: "category", label: "Categorie" },
+        { key: "associations", label: "Associations" },
+        { key: "actions", label: "Actions" },
+      ],
 
-    funds: "",
-    employees_number: "",
-    jobs_available_number: "",
-    women_number: "",
-    revenues: "",
-    position: "",
-    latitude: "",
-    longitude: "",
-  }),
-
-  /* Permet de regrouper les differents element lié a la position dans la variable adress */
-  watch: {
-    streetName: function() {
-      this.adress = this.streetNumber + " " + this.streetName;
-    },
-
-    streetNumber: function() {
-      this.adress = this.streetNumber + " " + this.streetName;
-    },
+      // Tableau récupérations demande modification
+      buffers: [],
+    };
   },
 
-  methods: {
-    getPosition() {
-      /* requete afin de recupérer les coordonnées GPS des adresses */
-
-      this.axios
-        .get(
-          `https://api-adresse.data.gouv.fr/search/?q=${this.streetNumber}+${this.streetName}+${this.city}+${this.postal_code}%22`
-        )
-
-        .then((response) => {
-          this.longitude = response.data.features[0].properties.x;
-          this.latitude = response.data.features[0].properties.y;
-        });
-
-      console.log(this.longitude);
-    },
-
-    register() {
-      this.axios
-        .post(this.baseUrl + "api/POST/register", {
-          /* body de la requete */
-
-          name: this.name,
-          email: this.email,
-          logo: this.logo,
-          adress: this.adress,
-          postal_code: this.postal_code,
-          city: this.city,
-          longitude: this.longitude,
-          latitude: this.latitude,
-          phone: this.phone,
-          category: this.category,
-          associations: this.associations,
-          description: this.description,
-          facebook: this.facebook,
-          twitter: this.twitter,
-          linkedin: this.linkedin,
-          website: this.website,
-          activity_area: this.activity_area,
-          funds: this.funds,
-          employees_number: this.employees_number,
-          jobs_available_number: this.employees_number,
-          women_number: this.women_number,
-          revenues: this.revenues,
-        })
-
-        .then((response) => console.log(response));
-    },
-
-    /* methode transformer le logo en base 64 pour la BDD */
-
-    addLogo(e) {
-      const reader = new FileReader();
-      reader.onload = (readerEvent) => {
-        this.logo = readerEvent.target.result;
-      };
-      if (e.target.files[0].size / 1024 / 1024 > 3) {
-        console.log("image trop grande");
-      } else {
-        reader.readAsDataURL(e.target.files[0]);
-      }
-    },
+  // ?Récupération
+  beforeMount() {
+    this.axios
+      .get(this.baseUrl + "api/admin/GET/update/demande")
+      .then((response) => {
+        for (const elem of response.data.body.buffers) {
+          this.buffers.push(elem);
+        }
+        // console.log(this.admins);
+      });
   },
+  methods: {},
 };
 </script>
 
-<style lang="scss">
-#register {
-  width: 80%;
-  margin: auto;
-
-  h4 {
-    text-align: center;
-    margin: 20px;
-    margin-top: 30px;
-  }
-
-  label {
-    width: 100%;
-    text-align: left;
-  }
-  #buttonSubmit {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    margin-bottom: 20px;
-  }
-}
-</style>
+<style lang="sass"></style>
